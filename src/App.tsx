209a33dce35e52d36f86d0de6625c3035e86515c
David Caro Gallego <carogallegodavid@gmail.com>
2024-02-28 16:58:44 +0100

    feat: install and configure i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { HelmetProvider } from 'react-helmet-async';
+import i18n, { InitOptions } from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRoutes } from './routes';
+import * as locales from './locales';
 import './theme/global.css';
+
+const i18nOptions: InitOptions = {
+  resources: locales,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+};
+i18n.use(initReactI18next).init(i18nOptions);
 
 const queryClient = new QueryClient();
 
