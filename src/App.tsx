ed8a6e7209752377f436df5b5b310c01d4bc499b
David Caro Gallego <carogallegodavid@gmail.com>
2024-02-29 20:06:46 +0100

    feat: add React Query devtools and tsc script

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import i18n, { InitOptions } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppRoutes } from '@/routes';
 import * as locales from '@/locales';
 import './theme/global.css';
@@ -14,11 +15,18 @@ const i18nOptions: InitOptions = {
 };
 i18n.use(initReactI18next).init(i18nOptions);
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, // Cache results to avoid reaching the API rate limit
+    },
+  },
+});
 
 const App = () => (
   <HelmetProvider>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <AppRoutes />
     </QueryClientProvider>
   </HelmetProvider>
