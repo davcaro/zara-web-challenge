5de1eb6441757f7825adce997db764d95ccac00a
David Caro Gallego <carogallegodavid@gmail.com>
2024-03-04 18:26:11 +0100

    feat(use-favorite-characters): install fuse and filter favorite characters

--- src/stores/FavoriteCharactersContext/useFavoriteCharacters.ts
+++ src/stores/FavoriteCharactersContext/useFavoriteCharacters.ts
@@ -1,12 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
+import Fuse from 'fuse.js';
 import { FavoriteCharactersContext } from './FavoriteCharactersContext';
+import { Character } from '@/types';
 
-export const useFavoriteCharacters = () => {
+const SEARCH_OPTIONS = {
+  keys: ['name'],
+  threshold: 0.4,
+};
+
+export const useFavoriteCharacters = (search?: string) => {
   const context = useContext(FavoriteCharactersContext);
 
   if (!context) {
     throw new Error('useFavoriteCharacters must be used within a FavoriteCharactersProvider');
   }
 
-  return context;
+  const filteredCharacters = useMemo(() => {
+    if (!search?.length) {
+      return context.favorites;
+    }
+
+    const fuseList = new Fuse(context.favorites, SEARCH_OPTIONS);
+    return fuseList.search(search).map(({ item }: { item: Character }) => item);
+  }, [context.favorites, search]);
+
+  return { ...context, favorites: filteredCharacters };
 };
